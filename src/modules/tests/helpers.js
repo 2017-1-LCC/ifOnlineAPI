757de0abe175e8cf0397c856e7c66acfc9827a5e
hc3 <eliel.floyd@bol.com.br>
2017-08-21 23:52:52 -0300

    iniciando os testes com TDD

--- src/modules/tests/helpers.js
+++ src/modules/tests/helpers.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest';
 import chai from 'chai';
-import app from '../../app';
+import app from '../../../app';
 
 global.app = app;
 global.request = supertest(app);
--- src/modules/tests/studentTest.js
+++ src/modules/tests/studentTest.js
@@ -1,18 +1,26 @@
+import model from '../student/Student';
 
-
-describe("## TESTE INTEGRATION # ROUTES -",() => {
+describe("## Testando as rotas de Student",() => {
 
   const defaultObj = {
-
+    nick:"hc3",
+    registration:"2201"
   }
 
   beforeEach(done => {
-
+    model.remove({})
+      .then(obj => obj)
+      .catch(err => err);
   })
 
   describe("TESTE DA ROTA /STUDENT", () => {
     it("Deve retornar uma lista com todos os dados",done => {
-      
+      request
+        .get('/student')
+        .end((err,res) => {
+          expect(res.body[0].nick).to.eql(defaultObj.name);
+          done(err);
+        })
     })
   })
 
