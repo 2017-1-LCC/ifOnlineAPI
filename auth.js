f81e4770c4df02da905711e2cfa8e3406fc05646
hc3 <eliel.floyd@bol.com.br>
2017-08-24 23:04:33 -0300

    iniciando implementação de jwt nas rotas

--- auth.js
+++ auth.js
@@ -1,71 +1,68 @@
 import UserService from './src/modules/user/UserService';
 import Bcrypt from 'bcrypt';
-
-exports.register = (server, options ,next) => {
-  server.register(require('hapi-auth-basic'),(err) => {
-
-      if(err) throw err;
-
-      const service = new UserService(server.app.db.models.user);
-
-      function basicValidation(request, username, password, callback) {
-
-        function success(user) {
-          if (!user) {
-              return callback(null, false)
-          }
-          
-          Bcrypt.compare(password, user[0].password, function (err, isValid) {
-              callback(err, isValid, { id: user.id, username: user.username })
-          })
-        };
-
-        function error(error) {
-          return error;
-        };
-
-        service.findByUsername(username,success,error);
-
-      }
-
-      server.auth.strategy('simple','basic',{ validateFunc: basicValidation });
-      next();
-  })
-}
-
 /*
 exports.register = (server, options ,next) => {
-  server.register(require('hapi-auth-basic'),(err) => {
+  server.register(require('hapi-auth-jwt'),(err) => {
 
       if(err) throw err;
 
       const service = new UserService(server.app.db.models.user);
 
-      function basicValidation(request, username, password, callback) {
+      function validate(request, decodedToken, callback) {
 
-        function success(user) {
-          if (!user) {
-              return callback(null, false)
+          var error,
+              credentials = accounts[decodedToken.accountId] || {};
+
+          if (!credentials) {
+              return callback(error, false, credentials);
           }
-          
-          Bcrypt.compare(password, user[0].password, function (err, isValid) {
-              callback(err, isValid, { id: user.id, username: user.username })
-          })
-        };
 
-        function error(error) {
-          return error;
-        };
+          return callback(error, true, credentials)
+      };
 
-        service.findByUsername(username,success,error);
+      server.auth.strategy('token', 'jwt', {
+          key: 'secret',
+          validateFunc: validate,
+          verifyOptions: { algorithms: [ 'HS256' ] }  // only allow HS256 algorithm
+      });
+      //server.auth.strategy('simple','basic',{ validateFunc: basicValidation });
 
-      }
-
-      server.auth.strategy('simple','basic',{ validateFunc: basicValidation });
-      next();
   })
 }
 */
+
+exports.register = (server, options ,next) => {
+  server.register(require('hapi-auth-basic'),(err) => {
+
+      if(err) throw err;
+
+      const service = new UserService(server.app.db.models.user);
+
+      function basicValidation(request, username, password, callback) {
+
+        function success(user) {
+          if (!user) {
+              return callback(null, false)
+          }
+          
+          Bcrypt.compare(password, user[0].password, function (err, isValid) {
+              callback(err, isValid, { id: user.id, username: user.username })
+          })
+        };
+
+        function error(error) {
+          return error;
+        };
+
+        service.findByUsername(username,success,error);
+
+      }
+
+      server.auth.strategy('simple','basic',{ validateFunc: basicValidation });
+      next();
+  })
+}
+
 
 
 
