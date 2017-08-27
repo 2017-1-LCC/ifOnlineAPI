1b099330eea8f35f248da46bc9b66f057605c398
hc3 <eliel.floyd@bol.com.br>
2017-08-27 20:47:11 -0300

    enviando

--- src/modules/config/db.js
+++ src/modules/config/db.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const DB = 'ifonline'
 const DB_UMBLER = 'mongodb://hc3:ifonline2017@tatooine.mongodb.umbler.com:40952/ifonline'
 const heroku = 'mongodb://hc3:root@ds025973.mlab.com:25973/sgvdb'
-mongoose.connect(DB_UMBLER)
+mongoose.connect(heroku)
 mongoose.Promise = require('bluebird')
 
 const db = mongoose.connection
 
 db.on('error', (err) => console.log('Erro de conexao.', err) )
 db.on('open', () => console.log('Conexão aberta.') )
-db.on('connected', (err) => console.log('Conectado na base: ', DB_UMBLER) )
+db.on('connected', (err) => console.log('Conectado na base: ', heroku) )
 db.on('disconnected', (err) => console.log('Desconectado') )
 
 export default db;
