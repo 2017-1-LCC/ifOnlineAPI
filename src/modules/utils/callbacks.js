0ee9e5d0473a3a0b038121d7ec62b2aae0e7e54e
hc3 <eliel.floyd@bol.com.br>
2017-09-19 15:28:24 -0300

    enviand ateração nos callbacks

--- src/modules/utils/callbacks.js
+++ src/modules/utils/callbacks.js
@@ -3,6 +3,28 @@ import sucessToken from './success-token';
 import errorToken from './error-token';
 import error from './error';
 
+
+//REFATORAR PASSAR SÓ O REQUEST COMO ARGUMENTO
+/* 
+* DEIXAR A CHAMADA ASSIM service.method(request, success(reply), error(reply))
+* REFATORAR TODOS OS SERVICES
+* ORGANIZAR NOME DAS AÇÕES
+* TROCAR NOME DA PASTA DE CALLBACKS PARA ACTIONS
+
+const makeRequest = ({req, res, service}) => ({
+    find: service.listAll(success(reply),error(reply)),
+    findById: service.listById(request.params.id,success(reply),error(reply)),
+    insert: service.create(request.payload,success(reply),error(reply)),
+    remove: service.remove(request.params.id,success(reply),error(reply)),
+    update: service.update(request.params.id,request.payload,success(reply),error(reply)),
+    createToken: service.createToken(request.payload,sucessToken(reply,request.payload),errorToken(reply)),
+    deleteToken: service.deleteToken(request.payload,sucessToken(reply),errorToken(reply)),
+    findByUser: service.findByUser(request.params.id,success(reply),error(reply)),
+    findGroupsWithPopulate: service.findWithPopulate(request.params.id,success(reply), error(reply)),
+    findGroupsByTeacher: service.findByTeacher(request.params.name, success(reply), error(reply)),
+    findPopulate: service.findPopulate(success(reply),error(reply))
+})
+*/
 const obj = {
 
     find: (request, reply, service) => {
