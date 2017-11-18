69972169d32e1b78dbedbfc2c2bce0a4a5dbfb26
hc2 <eliel.floyd@bol.com.br>
2017-11-18 21:07:18 -0200

    atividade programada no grupo de estudo.

--- src/modules/studygroup/StudyGroup.js
+++ src/modules/studygroup/StudyGroup.js
@@ -56,32 +56,14 @@ const obj = new Schema({
     }]
 });
 
-
 obj.pre('findOneAndUpdate',function(next) {
     var group = this;
-    //console.log("to aqui",group._update.classSchedule);
+    
     group._update.classSchedule = group._update.classSchedule.filter(el => !el.removed );
     group._update.proof = group._update.proof.filter(el => !el.removed );
-    
+    group._update.scheduledActivity = group._update.scheduledActivity.filter(el => !el.removed );
+
     next();
 })
-
-/*
-
-
-turma:{2017.1},
-disciplina:{ppp1},
-data_inicio:{data em que inicia a disciplina},
-data_final:{data em que se encerra a disciplina},
-provas:[{
-  assuntos:[assunto1, assunto2, assunto3],
-  data:{data da prova},
-  peso:{prova vale 10?}
-}],
-
-
-*/
-
-
 
 export default mongoose.model('studygroup',obj);
