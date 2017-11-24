6d6b07f7e840daafc1bb8be43cdb872520796b76
hc2 <eliel.floyd@bol.com.br>
2017-11-23 23:17:19 -0200

    testando erros on heroku

--- src/modules/actions/error.js
+++ src/modules/actions/error.js
@@ -1,9 +1,9 @@
 export default (reply) => (err) => {
   console.log("deu erro!",err.errors);
   if(err.errors.username) {
-    return reply({ message:err.errors.username.message });
+    return reply(err.errors.username.message);
   } else if(err.errors.email) {
-    return reply({ message:err.errors.email.message} );
+    return reply({ message:err.errors.email.message });
   } else {
     return reply(err);
   }
